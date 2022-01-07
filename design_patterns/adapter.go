f4ca8f2d530d28ffd0e292189ea06629a5502ac0
Machuca Juan <machucajuangabriel@gmail.com>
2022-01-07 09:14:51 -0300

    adapter

---
+++ design_patterns/adapter.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+type Payment interface {
+	Pay()
+}
+
+func ProccessPayment(p Payment) {
+	p.Pay()
+}
+
+type CashPayment struct{}
+
+func (CashPayment) Pay() {
+	fmt.Println("Payment using Cash")
+}
+
+type BankPayment struct{}
+
+func (BankPayment) Pay(bankAccount int) {
+	fmt.Printf("Paying using BankAccount %d\n", bankAccount)
+}
+
+/*Soluciòn*/
+type BankPaymentAdapter struct {
+	BankPayment *BankPayment
+	bankAccount int
+}
+
+func (bpa *BankPaymentAdapter) Pay() { //Adaptacion
+	bpa.BankPayment.Pay(bpa.bankAccount)
+}
+
+func main() {
+	cash := &CashPayment{}
+	ProccessPayment(cash)
+
+	//bank no puede implementar correctamente Pay
+	//bank := &BankPayment{}
+	//ProccessPayment(bank.)
+	// al ejecutar esto arroja un error
+
+	// *****************************
+	bpa := &BankPaymentAdapter{
+		bankAccount: 5,
+		BankPayment: &BankPayment{},
+	}
+	ProccessPayment(bpa)
+
+}
